728782e46566032cdcb2101eb1f1ce5f00946b40
Anton Boiko <snailonrail@gmail.com>
2020-03-03 11:59:54 +0200

    init

---
+++ models/animal.py
@@ -0,0 +1,55 @@
+import json
+
+from config import db
+from models.center import Center
+
+
+class Animal(db.Model):
+    __tablename__ = "animal"
+    animal_id = db.Column(db.Integer, primary_key=True)
+    center_id = db.Column(db.Integer, db.ForeignKey("center.id"))
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    spicie = db.Column(db.String, nullable=True)
+
+    def __repr__(self):
+        animal_object = {
+            'center_id': self.center_id,
+            'name': self.name,
+            'age': self.age,
+            'spicie': self.spicie,
+            'id': self.animal_id
+        }
+        return json.dumps(animal_object)
+
+
+def get_all_animals():
+    return Animal.query.all()
+
+
+def delete_animal(_animal_id):
+    animal = Animal.query.filter_by(animal_id=_animal_id).first()
+    db.session.delete(animal)
+    db.session.commit()
+
+
+def get_animal(_animal_id):
+    return Animal.query.filter_by(animal_id=_animal_id).first()
+
+
+def add_animal(_center_id, _name, _age, _spicie):
+    r_center = Center.query.filter(Center.id == _center_id).one_or_none()
+    new_animal = Animal(center=r_center, name=_name, age=_age, spicie=_spicie)
+
+    db.session.add(new_animal)
+    db.session.commit()
+
+
+def update_animal(_animal_id, animal):
+    existed_animal = Animal.query.filter_by(animal_id=_animal_id).first()
+    existed_animal.name = animal.name
+    existed_animal.age = animal.age
+    existed_animal.spicie = animal.spicie
+    existed_animal.center_id = animal.center_id
+    db.session.add(existed_animal)
+    db.session.commit()
---
+++ models/center.py
@@ -0,0 +1,42 @@
+from config import db
+import json
+#from models.animal import Animal
+
+
+def get_all_centers():
+    return Center.query.all()
+
+
+def get_center(_center_id):
+    return Center.query.filter_by(id=_center_id).first()
+
+
+def add_center(_login, _password, _address):
+    new_center = Center(login=_login, password=_password, address=_address)
+    db.session.add(new_center)
+    db.session.commit()
+
+
+class Center(db.Model):
+    __tablename__ = "center"
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(32), nullable=False)
+    password = db.Column(db.String(32), nullable=False)
+    address = db.Column(db.String(32))
+
+    animals = db.relationship(
+        "Animal",
+        backref="center",
+        cascade="all, delete, save-update, delete-orphan",
+        single_parent=True,
+    )
+
+    def __repr__(self):
+        center_object = {
+            'login': self.login,
+            'address': self.address,
+            # 'animals': self.animals
+        }
+        return json.dumps(center_object)
+
+
