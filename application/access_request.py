2ad12d9741383289fb9151724bfe4f5e4add2df1
aboiko1 <aboiko@griddynamics.com>
2020-03-05 18:37:29 +0200

    access req to be finished

--- application/access_request.py
+++ application/access_request.py
@@ -1,7 +1,8 @@
 from . import db
 
+
 class Access_Request(db.Model):
     __tablename__ = "access request"
     id = db.Column(db.Integer, primary_key=True)
-    center_id = db.Column(db.Integer, db.ForeignKey("center.id"))
+    center_id = db.Column(db.Integer, nullable=False)
     timestamp = db.Column(db.DateTime, nullable=False)
