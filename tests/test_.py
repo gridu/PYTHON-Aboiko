1dd5381491a7653fd6f0fe9fa05021505ad48551
Anton Boiko <snailonrail@gmail.com>
2020-03-13 11:18:50 +0200

    added tests

--- tests/test_.py
+++ tests/test_.py
@@ -34,11 +34,6 @@ def test_register(client, session):
     compare_digest(r.password, generate_hash(test_password_register))
 
 
-def test_token_required(client):
-    response = client.get("/animals")
-    assert response.status_code == 401
-
-
 def test_get_animals(client):
     animals_response = client.get("/animals", headers={'x-access-token': 'wrong_value'})
     assert animals_response.json['error'] == 'Token is invalid!'
@@ -51,6 +46,7 @@ def test_get_animals(client):
     token = get_test_token(client)
     animals_response = client.get("/animals", headers={'x-access-token': token})
     assert animals_response.status_code == 200
+    assert len(animals_response.get_json()['animals']) == 3
 
 
 def test_post_animal(client, session):
@@ -62,17 +58,28 @@ def test_post_animal(client, session):
                                          "specie": "not exists"})
     assert animals_response.status_code == 409
 
-    # correct request body
-    animals_response = client.post("/animals", headers={'x-access-token': token},
-                                   json={"name": test_animal_name, "age": test_animal_age,
-                                         "specie": test_animal_specie})
-    assert animals_response.status_code == 201
-
     # malformed JSON in request body
     animals_response = client.post("/animals", headers={'x-access-token': token},
                                    json={"nam": test_animal_name, "age": test_animal_age,
                                          "specie": test_animal_specie})
     assert animals_response.status_code == 400
+
+    # correct request body
+    result = session.execute("select * from animal where name = :value", {'value': test_animal_name})
+    rows = result.fetchall()
+    assert len(rows) == 0
+
+    animals_response = client.post("/animals", headers={'x-access-token': token},
+                                   json={"name": test_animal_name, "age": test_animal_age,
+                                         "specie": test_animal_specie})
+    assert animals_response.status_code == 201
+
+    # get just added animal from database with raw SQL query
+    result = session.execute("select * from animal where name = :value", {'value': test_animal_name})
+    rows = result.fetchall()
+    assert rows[0].age == test_animal_age and \
+           rows[0].specie == test_animal_specie and \
+           rows[0].name == test_animal_name
 
     # post already existing animal (retry the same request in the same test)
     animals_response = client.post("/animals", headers={'x-access-token': token},
@@ -80,26 +87,60 @@ def test_post_animal(client, session):
                                          "specie": test_animal_specie})
     assert animals_response.status_code == 409
 
-    # get just added animal from database with raw SQL query
-    result = session.execute("select * from animal where name = :value", {'value': test_animal_name})
-    Record = namedtuple('Record', result.keys())
-    records = [Record(*r) for r in result.fetchall()]
-    r = records[0]
-    assert r.age == test_animal_age
-    assert r.specie == test_animal_specie
 
-
-def test_get_animal_for_the_center(client):
+def test_get_animal_for_the_center(client, session):
     token = get_test_token(client)
-    animals_response = client.get("/animals/3", headers={'x-access-token': token})
-    assert animals_response.status_code == 200
+    animals_response = client.get("/animals/" + str(test_animal_id_for_login), headers={'x-access-token': token})
+    result = session.execute("select * from animal where id = :value", {'value': test_animal_id_for_login})
+    rows = result.fetchall()
+    assert animals_response.get_json()['name'] == rows[0].name and \
+           animals_response.get_json()['age'] == rows[0].age and \
+           animals_response.get_json()['specie'] == rows[0].specie and \
+           animals_response.status_code == 200
 
     # trying to get an animal from another center collection
     animals_response = client.get("/animals/4", headers={'x-access-token': token})
-    assert animals_response.status_code == 409
-    assert animals_response.json['error'] == 'you\'re trying to view the animal which isn\'t related to your id'
+    assert animals_response.status_code == 409 and \
+           animals_response.json['error'] == 'you\'re trying to view the animal which isn\'t related to your id'
 
     # trying to get not existing animal
     animals_response = client.get("/animals/25", headers={'x-access-token': token})
-    assert animals_response.status_code == 404
-    assert animals_response.json['error'] == 'you\'re looking for an animal which doesn\'t exist'
+    assert animals_response.status_code == 404 and \
+           animals_response.json['error'] == 'you\'re looking for an animal which doesn\'t exist'
+
+
+def test_delete_animal(client, session):
+    token = get_test_token(client)
+    animals_response = client.delete("/animals/3", headers={'x-access-token': token})
+    assert animals_response.status_code == 200 and \
+           animals_response.json['success'] == "an animal deleted successfully"
+    result = session.execute("select * from animal where id = :value", {'value': 3})
+    rows = result.fetchall()
+    assert len(rows) == 0
+
+    animals_response = client.delete("/animals/4", headers={'x-access-token': token})
+    assert animals_response.status_code == 409 and \
+           animals_response.json['error'] == 'you\'re trying to delete an animal which isn\'t related to your id'
+
+    animals_response = client.delete("/animals/44", headers={'x-access-token': token})
+    assert animals_response.status_code == 404 and \
+           animals_response.json['error'] == 'you\'re trying to delete an animal which doesn\'t exist'
+
+
+def test_update_animal(client, session):
+    token = get_test_token(client)
+    result = session.execute("select * from animal where id = :value", {'value': test_animal_id_for_login})
+    rows = result.fetchall()
+    name_before = rows[0].name
+    age_before = rows[0].age
+    specie_before = rows[0].specie
+    response = client.put("/animals/" + str(test_animal_id_for_login), json={"name": test_animal_name,
+                                                                             "age": test_animal_age,
+                                                                             "specie": test_animal_specie},
+                          headers={'x-access-token': token})
+    result = session.execute("select * from animal where id = :value", {'value': test_animal_id_for_login})
+    rows = result.fetchall()
+    assert name_before != rows[0].name \
+           and age_before != rows[0].age \
+           and specie_before != rows[0].specie
+
--- tests/test_data.py
+++ tests/test_data.py
@@ -4,7 +4,9 @@ test_address = "Berlin"
 
 test_login = "Center1"
 test_password = "password1"
+test_animal_id_for_login =2
 
 test_animal_name = "Gregg"
-test_animal_age = 3
+test_animal_name_post = "Animal for post"
+test_animal_age = 5
 test_animal_specie = "red fox"
